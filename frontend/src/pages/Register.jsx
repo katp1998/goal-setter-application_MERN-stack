17543da40cd9733829446607834307a6560b8a5d
Kethmie Perera <47343068+katp1998@users.noreply.github.com>
2022-07-25 07:48:02 +0530

    Login and Register FE
    Signed-off-by: Kethmie Perera <47343068+katp1998@users.noreply.github.com>

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,8 +12,15 @@ function Register() {
 
   const {name, email, password, confirmPassword} = formData
 
-  const onChange = () =>{
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value
+    }))
+  }
 
+  const onSubmit = (e) => {
+    e.preventDefault()
   }
 
   return (
@@ -25,7 +32,7 @@ function Register() {
         </h1>
     </section>
     <section className="form">
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
                 <input type="text" className='form-control' id = "name" name = "name" value={name} placeholder="Enter your name" onChange={onChange}/>
             </div>
@@ -33,10 +40,13 @@ function Register() {
                 <input type="text" className='form-control' id = "email" name = "email" value={email} placeholder="Enter your email address" onChange={onChange}/>
             </div>
             <div className="form-group">
-                <input type="text" className='form-control' id = "password" name = "password" value={password} placeholder="Enter your password" onChange={onChange}/>
+                <input type="password" className='form-control' id = "password" name = "password" value={password} placeholder="Enter your password" onChange={onChange}/>
             </div>
             <div className="form-group">
-                <input type="text" className='form-control' id = "confirmPassword" name = "confirmPassword" value={confirmPassword} placeholder="Confirm password" onChange={onChange}/>
+                <input type="password" className='form-control' id = "confirmPassword" name = "confirmPassword" value={confirmPassword} placeholder="Confirm password" onChange={onChange}/>
+            </div>
+            <div className='form-group'>
+                <button type="submit" className='btn btn-block'>Submit</button>
             </div>
         </form>
     </section>
